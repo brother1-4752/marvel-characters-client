af7c25b257a5ba961407250ddfbcf94b0b112ff6
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-05-23 21:04:52 +0900

    gh-page url 수정

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  //deployment
+  uri: "https://kfeyqcx4j8.execute-api.us-east-1.amazonaws.com/",
+  //local
+  // uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
